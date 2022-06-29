ae204e2bc75b48a71f3ade7dd9a1cf54c0d9654b
Aiden Tong <93221974+Meguumin@users.noreply.github.com>
2022-06-29 00:07:07 +0000

    Added sound

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 let t;
 let tic;
+var kaching = new Audio('kaching.mp3');
+var wah = new Audio('wah.mp3');
+var click = new Audio('click.mp3');
 tic = 0;
 t = 20;
 document.getElementById("output").innerHTML = "You earned " + tic + "$";
 let element = document.getElementById("id01");
 element.innerHTML = "Total Money:" + t; 
 const counters = document.querySelectorAll('.counter');
-
+	let earned = document.Money;
+ wah.volume = 0.4;
 function perform(totalmoney, moneywon, moneygamble, moneymax)
 { 
+ 
+  click.play();
   if(totalmoney >= moneygamble)
   {
     totalmoney = totalmoney - moneygamble;
@@ -19,7 +25,16 @@ function perform(totalmoney, moneywon, moneygamble, moneymax)
 	  const element = document.getElementById("id01");
 	  element.innerHTML = "Total Money:" + totalmoney;
     t = totalmoney;
+    if(moneywon > moneygamble)
+    {
+      kaching.play();
+    }
+    else{
+       wah.play();
+    }
+    
   }
+  
 
   // Counting animation
   var Money = document.querySelector(".Money")
@@ -28,7 +43,6 @@ function counterUp(){
     Money.innerHTML = "You earned " + countOne+ "$"
     countOne++
     if(countOne > moneywon){
-
         clearInterval(stop)
     }
 }
