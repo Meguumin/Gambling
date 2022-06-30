ab32ba215491ffb64a169e94be41189ba06a8a72
Aiden Tong <93221974+Meguumin@users.noreply.github.com>
2022-06-30 00:37:40 +0000

    add sfx toggle

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let t;
 let tic;
+var sfx;
 var kaching = new Audio('kaching.mp3');
 var wah = new Audio('wah.mp3');
 var click = new Audio('click.mp3');
+sfx = 1;
 tic = 0;
 t = 20;
 document.getElementById("output").innerHTML = "You earned " + tic + "$";
@@ -55,11 +57,31 @@ var stop = setInterval(function(){
 
 function ChangeTheme() 
 { 
+  click.play();
 	let element = document.body;
 	element.classList.toggle("coral");
 }
 
-
+function sfxstate() 
+{ 
+  click.play();
+ if (sfx == 0)//false
+ {
+   kaching.volume = 1;
+   click.volume = 1;
+   wah.volume = 0.4;
+   sfx = 1;
+   return;
+ }
+  if (sfx == 1)//true
+ {
+   kaching.volume = 0;
+   click.volume = 0;
+   wah.volume = 0;
+   sfx = 0;
+   return;
+ }
+}
 
 
 
